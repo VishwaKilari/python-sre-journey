53d525b730f8198c889b94db5b3edef667a9b5f7
VishwaKilari <vishwakilari@gmail.com>
2026-05-02 22:45:34 +0530

    Phase 1: variables and data types in basics.py

---
+++ basics.py
@@ -0,0 +1,18 @@
+# Variables and Data Types
+# Author : Vishwa
+
+name = "Vishwa"
+age = 25
+height = 5.9
+is_engineer = True
+
+print(name)
+print(age)
+print(height)
+print(is_engineer)
+print(25+10)
+print("25"+"10")
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_engineer))
