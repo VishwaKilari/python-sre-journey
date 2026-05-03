501e680347cf5aab45c86892cbfda57b72fa9622
VishwaKilari <vishwakilari@gmail.com>
2026-05-03 17:13:50 +0530

    Phase 1: user input, type conversion, f-strings

--- basics.py
+++ basics.py
@@ -6,6 +6,7 @@ age = 25
 height = 5.9
 is_engineer = True
 
+'''
 print(name)
 print(age)
 print(height)
@@ -16,3 +17,10 @@ print(type(name))
 print(type(age))
 print(type(height))
 print(type(is_engineer))
+'''
+user_name = input("what is your name?\n")
+user_age = int(input("what is your age?\n"))
+remaining_years = 60 - (user_age)
+#print(f"Hello {user_name}, you are {user_age} years old and you have {remaining_years} years until you are 60")
+#print(remaining_years)
+print("Hello "+ str(user_name) + ", you are " + str(user_age) + " years old and you have " + str(remaining_years) + " years until you are 60")
